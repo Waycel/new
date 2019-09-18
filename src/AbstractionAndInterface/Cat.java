f4f6225bd71eb157289db1ee6cd3b4213af2f1fc
Wecl-19 <54606295+Wecl-19@users.noreply.github.com>
2019-09-18 12:53:08 -0500

    Update Cat.java

--- src/AbstractionAndInterface/Cat.java
+++ src/AbstractionAndInterface/Cat.java
@@ -3,5 +3,7 @@ package AbstractionAndInterface;
 public class Cat extends Animal {
     public void makeSound(){
         System.out.println("meow meow");
+        System.out.println("meow meow");
+
     }
 }
